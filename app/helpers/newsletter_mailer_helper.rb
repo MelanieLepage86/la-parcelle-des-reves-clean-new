a45b0efd912f7710106650b6441885c6cdb8aab8
melanie lepage <melanie.lepage86@hotmail.fr>
2025-11-06 20:20:32 +0100

    newsletter cloudinary v4

--- app/helpers/newsletter_mailer_helper.rb
+++ app/helpers/newsletter_mailer_helper.rb
@@ -2,12 +2,8 @@ module NewsletterMailerHelper
   include Rails.application.routes.url_helpers
 
   def public_image_url(attachment)
-    return nil unless attachment.present? && attachment.blob.present?
+    return nil unless attachment.present? && attachment.attached?
 
-    if Rails.application.config.active_storage.service == :cloudinary
-      attachment.service_url
-    else
-      rails_blob_url(attachment, host: Rails.application.routes.default_url_options[:host], protocol: Rails.application.routes.default_url_options[:protocol])
-    end
+    rails_blob_url(attachment, host: Rails.application.routes.default_url_options[:host], protocol: 'https')
   end
 end
