078fd0afcec6df4d7cf0804ded89da825a959165
melanie lepage <melanie.lepage86@hotmail.fr>
2025-11-06 19:05:35 +0100

    contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,12 @@
 class ContactMailer < ApplicationMailer
   default to: 'laparcelledesreves.art@gmail.com'
 
-  def new_message(contact)
-    @contact = contact
-    mail(subject: "Nouveau message de #{@contact.name}", from: 'laparcelle@mg.laparcelledesreves.com')
+  def new_message(contact_message)
+    @contact_message = contact_message
+    mail(
+      subject: "Nouveau message de #{@contact_message.name}",
+      from: 'laparcelle@mg.laparcelledesreves.com'
+    )
   end
 end
+
