08c9f40f6670d16cea602c4519c40ee4bf04bebf
melanie lepage <melanie.lepage86@hotmail.fr>
2025-11-07 13:46:43 +0100

    newsletter cloudinary v8

--- app/helpers/newsletter_mailer_helper.rb
+++ app/helpers/newsletter_mailer_helper.rb
@@ -2,6 +2,6 @@ module NewsletterMailerHelper
   def public_image_url(attachment)
     return nil unless attachment.present? && attachment.blob.present?
 
-    attachment.blob.service_url
+    attachment.url
   end
 end
